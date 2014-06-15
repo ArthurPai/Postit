64c6ea13771e6c8bd3e3df81502a7d25dfed684a
ArthurPai <baidragoon@gmail.com>
2014-06-15 23:49:18 +0800

    using application helper to display user friendly time format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
   def fix_url(url)
     url.start_with?('http://') ? url : "http://#{url}"
   end
+
+  def custom_time_ago_in_words(time)
+    "#{time_ago_in_words(time)} ago"
+  end
+
+  def display_datetime(dt)
+    dt.strftime("%m/%d/%Y %l:%M%P %Z")
+  end
 end
