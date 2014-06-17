b71c1023bc05a07786f154e2c5fd76d131b7d2e1
ArthurPai <baidragoon@gmail.com>
2014-06-17 17:49:17 +0800

    implement user register

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
+  get '/register', to: 'users#new'
+
   resources :posts, except: [:destroy] do
     resources :comments, only: [:create]
   end
 
   resources :categories, only: [:show, :new, :create]
+  resources :users, only: [:create]
 end
