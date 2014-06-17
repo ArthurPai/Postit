8486344be3f6a74fa89f17cedf2ed0c22f7330cb
ArthurPai <baidragoon@gmail.com>
2014-06-17 21:35:09 +0800

    add feature let user auto login when register success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController <ApplicationController
 
     if @user.save
       flash[:notice] = "Yor are register success."
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render :new
