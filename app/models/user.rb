5af6dc961fcfb23c006b9a64b7dc4ec5440e0170
ArthurPai <baidragoon@gmail.com>
2014-06-18 17:01:06 +0800

    restrict user can only vote on a post/comment once

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :votes
 
+  has_secure_password validations: false
+
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 4 }, on: :create
   validates :password, allow_blank: true, length: { minimum: 4 }, on: :update
-  has_secure_password validations: false
 end
