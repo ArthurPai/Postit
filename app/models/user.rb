f29cafe8d70069845311aec111aa9051f5ec027f
ArthurPai <baidragoon@gmail.com>
2014-06-21 16:25:37 +0800

    add user slug url

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,14 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 4 }, on: :create
   validates :password, allow_blank: true, length: { minimum: 4 }, on: :update
+
+  after_validation :generate_slug
+
+  def generate_slug
+    self.slug = "#{self.id}-" + username.to_slug
+  end
+
+  def to_param
+    slug
+  end
 end
