5c65517020c2fc8527dfbdbfd344bfc8bbf00907
ArthurPai <baidragoon@gmail.com>
2014-06-21 13:51:19 +0800

    fix crach when submit blank comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,7 @@ class CommentsController < ApplicationController
       flash[:notice] = 'Your comment was added.'
       redirect_to post_path(@post)
     else
+      @post.reload
       render 'posts/show'
     end
   end
