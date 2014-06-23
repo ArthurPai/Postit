454e22713b77ab22fe3e30249f2011a41eee3312
ArthurPai <baidragoon@gmail.com>
2014-06-23 19:28:43 +0800

    add user time zone

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,13 +12,17 @@ module ApplicationHelper
     difference_day = (Time.zone.now.to_date - time.to_date).to_i
 
     if difference_day >= 1
-      "at #{time.strftime("%d %b %Y - %H:%M:%S %Z")}"
+      if logged_in? && !current_user.time_zone.blank?
+        time = time.in_time_zone(current_user.time_zone)
+      end
+
+      "at #{time.strftime("%m/%d/%Y %H:%M:%S %Z")}" #06/23/2014 23:20:59 UTC
     else
       "#{time_ago_in_words(time)} ago"
     end
   end
 
-  def display_datetime(dt)
-    dt.strftime("%m/%d/%Y %l:%M%P %Z")
+  def asia_zones
+    @asia_zones ||= ActiveSupport::TimeZone.all.find_all { |z| z.name =~ /Taipei|Beijing|Hong Kong|Singapore|Osaka|Sapporo|Seoul|Tokyo/ }
   end
 end
